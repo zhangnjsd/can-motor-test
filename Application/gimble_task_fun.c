67534fd12a3c7f3c28d2b169989994e55b6a2cf2
zhangnjsd <zhangnjsd@qq.com>
2026-06-13 19:38:07 +0800

    update

--- Application/gimble_task_fun.c
+++ Application/gimble_task_fun.c
@@ -4,12 +4,14 @@
 
 void gimble_task_fun(void *argument) {
     i = 0;
+    uint32_t tick;
 
     // For transferring data to motor
     can_filter_init();
     int16_t vol[4] = {0};
     double t = 0;
     for (;;) {
+        tick = osKernelGetTickCount();
         i ++;
         if (i == UINT32_MAX) {
             i = 0;
@@ -19,6 +21,7 @@ void gimble_task_fun(void *argument) {
         vol[0] = (int16_t)(25000 * sin(t));
         CAN_Send(&hcan1, vol, CAN_ID);
 
-        osDelay(1);
+        //osDelay(1);
+        osDelayUntil(tick + 1);
     }
 }
