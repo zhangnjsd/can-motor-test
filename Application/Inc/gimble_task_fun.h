aa078562ea79a0d3323ea93136a46d78aae9a1ec
zhangnjsd <zhangnjsd@qq.com>
2026-06-14 21:38:01 +0800

    cge

--- Application/Inc/gimble_task_fun.h
+++ Application/Inc/gimble_task_fun.h
@@ -1,9 +1,15 @@
+#ifndef __GIMBLE_TASK_FUN_H
+#define __GIMBLE_TASK_FUN_H
+
 #include "bsp_can.h"
 #include "main.h"
-
 #include "cmsis_os.h"
 #include <limits.h>
 #include <math.h>
 #include "controller.h"
 
 #define CAN_ID 0x1FF
+
+void gimble_task_fun(void *argument);
+
+#endif /* __GIMBLE_TASK_FUN_H */
--- Application/Inc/recv_task_fun.h
+++ Application/Inc/recv_task_fun.h
@@ -1,6 +1,13 @@
+#ifndef __RECV_TASK_FUN_H
+#define __RECV_TASK_FUN_H
+
 #include "cmsis_os.h"
 #include <limits.h>
 #include "main.h"
 #include "bsp_can.h"
 #include <stdio.h>
 #include "gimble_task_fun.h"
+
+void recv_task_fun(void *argument);
+
+#endif /* __RECV_TASK_FUN_H */
